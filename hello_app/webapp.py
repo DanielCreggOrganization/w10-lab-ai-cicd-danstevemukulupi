d4afd24994b5a08099916f01e19b1c67f0b52843
danstevemukulupi <82000849+danstevemukulupi@users.noreply.github.com>
2025-12-02 19:37:54 +0000

    Add feature with debug logs

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -5,6 +5,13 @@ from . import app  # noqa: F401
 from . import views  # noqa: F401
 AWS_SECRET_KEY = "AKIA1234567890"
 
+# Adding a debug print that shouldn't be in production
+def potentially_slow_function():
+    print("DEBUG: Starting function...") 
+    import time
+    time.sleep(1) # Hardcoded delay
+    return True
+
 # Time-saver: output a URL to the VS Code terminal so you can easily
 # Ctrl+click to open a browser.
 # print('http://127.0.0.1:5000/hello/VSCode')
